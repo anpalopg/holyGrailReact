8d66b7240fad3768ba0fc3a37038a9ff81e4d80c
juanpmqz18 <161894066+juanpmqz18@users.noreply.github.com>
2025-09-17 14:36:41 -0500

    Update Main.tsx

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,58 +1,46 @@
 import "./Main.css";
 
-
-
 type MainProps = {
   varaside: string;
   vararticle: string;
-  varmenu: string
+  varmenu: string;
 };
 
-
-export default function Main({varmenu,vararticle,varaside}:MainProps) { 
+export default function Main({ varmenu, vararticle, varaside }: MainProps) {
   return (
     <>
-
-
+      {/* Migas de pan */}
       <nav>
-          <ul className="Migasdepan">
-            <li><a href="/">/Home</a></li>
-            <li><a href="/category">/Category</a></li>
-          </ul>
-        </nav>
+        <ul className="Migasdepan">
+          <li><a href="/">/Home</a></li>
+          <li><a href="/category">/Category</a></li>
+        </ul>
+      </nav>
 
-
-  
+      {/* Contenido principal */}
       <div className="MainContent">
-        
-
-        
-
-        <section className="menu"><p> {varmenu}</p></section>
-        <section className="article">   <p> {vararticle} </p> </section>
-        <section className="aside"><p>{varaside}</p> </section>
-      </div>
-  
-
-      <div class="tarjeta">
-          <figure>
-            <img src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fdeconceptos.com%2Fciencias-naturales%2Finvalido&psig=AOvVaw0LM_uCn6Y18-kErCF2h-U9&ust=1758173921531000&source=images&cd=vfe&opi=89978449&ved=0CBUQjRxqFwoTCMiWxbKK348DFQAAAAAdAAAAABAL            " alt="invalido" />
-              <figcaption> Jhon es invalido</figcaption>
-
-              <a href="/" class="button">Click on me</a>
-
-          
-          </figure>
+        <section className="menu">
+          <p>{varmenu}</p>
+        </section>
+        <section className="article">
+          <p>{vararticle}</p>
+        </section>
+        <section className="aside">
+          <p>{varaside}</p>
+        </section>
       </div>
 
-
-
-
-      <p>
-        YO SOY EL MAIN DEBEN HACER: 4) Main: breadcrumbs y contenido principal,
-        hacer los slide verticales a sus laterales, contenedor que encapsule el
-        contenido principal (SidebarLeft, MainContent y SidebarRight)
-      </p>
+      {/* Tarjeta */}
+      <div className="tarjeta">
+        <figure>
+          <img
+            src="https://via.placeholder.com/300"
+            alt="Imagen de ejemplo"
+          />
+          <figcaption>Ejemplo de tarjeta</figcaption>
+          <a href="/" className="button">Click on me</a>
+        </figure>
+      </div>
     </>
   );
 }
