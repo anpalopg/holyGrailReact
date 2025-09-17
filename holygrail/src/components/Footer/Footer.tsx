33c1ff2db69069a117f8b0d26fdb767fa6db7696
Jhon Perez <jhonysperez21@gmail.com>
2025-09-16 21:43:35 -0500

    Comienzo3

--- holygrail/src/components/Footer/Footer.tsx
+++
@@ -1,9 +0,0 @@
-import "./Footer.css";
-export default function Footer() {
-  return (
-    <>
-      <h2>Marlly</h2>
-      <p>YO SOY EL FOOTER, DEBEN AHCER: Informacion del copyrigth</p>
-    </>
-  );
-}
--- holygrail/src/main.tsx
+++
@@ -1,9 +0,0 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import HolyGrail from "./HolyGrail";
-
-createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <HolyGrail />
-  </StrictMode>
-);
